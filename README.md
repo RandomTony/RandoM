6235796fcae267174bce91440e69e77e5cbd7e97
RandomTony <anthony.lohou@laposte.net>
2016-03-18 23:27:15 +0100

    First interface

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Random Music playlist generator
 RandoM is a small application in python.
 Define your music directory, define the target directory (Your phone, HDD, ...).
 The app will generate a random playlist and copy it on your device.
+
+
+## TO DO
+File browser
+Random file selection
+copy/paste files
--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3.4
+
 import os
+from tkinter import *
 import shutil
 
-path = "./here/"
-AbsolPath = "./there/"
 
-# Add a new file
-def add(path,fileName):
-    shutil.copy2(os.path.join(path,fileName),os.path.join(target,fileName))
+def add(origin,fileName,target):
+    shutil.copy2(os.path.join(origin,fileName),os.path.join(target,fileName))
+
 
 # remove all the file in the Target directory
 def removeFiles(path):
@@ -19,4 +19,28 @@ def removeFiles(path):
     if path!=AbsolPath:
         os.rmdir(path)  # Remove directory current directory, except "root"
 
-#removeFiles(AbsolPath)
+
+
+def click():
+    if targetEnt.get()!="" and originEnt.get()!="":
+        origin = originEnt.get()
+        target = targetEnt.get()
+        add(origin, "hello.txt", target)
+
+
+frame = Tk()
+
+originLab = Label(frame, text="Origin").grid(row=0)
+originEnt = Entry(frame)
+originEnt.grid(row=0, column=1)
+
+targetLab = Label(frame, text="Target").grid(row=1)
+targetEnt = Entry(frame)
+targetEnt.grid(row=1, column=1)
+
+bouton_click = Button(frame, text="Copy", command=click).grid(row=2,column=1)
+bouton_quit = Button(frame, text="Quit", command=frame.quit).grid(row=2, column=0)
+
+frame.title("RandoM")
+frame.config(width=800, height=500)
+frame.mainloop()
