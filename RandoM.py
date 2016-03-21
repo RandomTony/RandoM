fef0ac81b1cb0155d75d272425a0f7b48ba444a9
RandomTony <anthony.lohou@laposte.net>
2016-03-21 11:42:29 +0100

    Show copied files

--- RandoM.py
+++ RandoM.py
@@ -29,6 +29,11 @@ class RandoM:
         self.bouton_click = Button(self.frame, text="Start", command=self.click).grid(row=2,column=2)
         self.bouton_quit = Button(self.frame, text="Quit", command=self.frame.quit).grid(row=4, column=0)
 
+        self.stat = StringVar()
+        self.stat.set("Waiting ... ")
+        self.statLab = Label(self.frame, textvariable=self.stat)
+        self.statLab.grid(row = 3)
+
         self.frame.title("RandoM")
         self.frame.config(width=800, height=500)
         self.frame.mainloop()
@@ -53,7 +58,7 @@ class RandoM:
                 os.remove(os.path.join(path, i))
             else:
                 removeFiles(os.path.join(path,i)) # If you find a new directory, remove it with the files in it
-        if path!=AbsolPath:
+        if path!=self.targetEnt.get():
             os.rmdir(path)  # Remove directory current directory, except "root"
 
     def countFile(self,path):
@@ -77,25 +82,30 @@ class RandoM:
     def copyAll(self,path):
         for i in os.listdir(path):
             if path!=self.targetEnt.get() and os.path.isfile(os.path.join(path,i)):
-                print(os.path.join(path,i))
+                self.stat.set(i)
                 self.add(path,i, self.targetEnt.get())
             elif os.path.isdir(os.path.join(path,i)):
                 self.copyAll(os.path.join(path,i))
+        if path!=self.targetEnt.get():
+            self.stat.set("Done.")
 
     def copyRandom(self):
         music = self.listFile(self.originEnt.get())
         for i in range(int(self.numberEntry.get())):
             n = int(random()*len(music))
-            print(os.path.basename(music[n]))
+            self.stat.set(os.path.basename(music[n]))
+            self.add(os.path.dirname(music[n]), os.path.basename(music[n]), self.targetEnt.get())
             music.remove(music[n])
+        self.stat.set("Done.")
 
 
     def click(self):
         if os.path.exists(self.targetEnt.get()) and os.path.exists(self.originEnt.get()):
             origin = self.originEnt.get()
             target = self.targetEnt.get()
-            print(self.numberEntry.get())
-            #self.add(origin, "hello.txt", target)
+
+            self.removeFiles(self.targetEnt.get())
+
             if self.countFile(origin)<=int(self.numberEntry.get()):
                 self.copyAll(self.originEnt.get())
             else:
